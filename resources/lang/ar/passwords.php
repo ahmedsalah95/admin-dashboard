3f88cca70b8073eeadb66ba05850517f3927b74c
ahmedsalah95 <ahmedsalah9570@gmail.com>
2023-11-29 10:15:16 +0200

    add all files

---
+++ resources/lang/ar/passwords.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password'      => 'يجب أن تكون كلمات المرور على الأقل من ستة أحرف وتتطابق مع التأكيد.',
+    'reset'         => 'تمت إعادة تعيين كلمة المرور الخاصة بك!',
+    'sent'          => 'لقد قمنا بإرسال رابط إعادة تعيين كلمة المرور إلى بريدك الإلكتروني!',
+    'token'         => 'رمز إعادة تعيين كلمة المرور هذا غير صالح.',
+    'user'          => 'لا يمكننا العثور على مستخدم بهذا البريد الإلكتروني.',
+    'change_error'  => 'كلمة المرور التي أدخلتها لا تتطابق مع كلمة المرور الحالية.',
+
+];
